9fa2da0214b4277b7b27e531917e0c7a4ce58478
Max Fierke <max@maxfierke.com>
2013-07-24 09:21:52 -0500

    Dynamic room list population and nav item highlighting.
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ assets/js/global.js
@@ -0,0 +1,13 @@
+// If rooms list exists, let's populate it.
+if($('.rooms-list').length > 0) {
+    $.getJSON('/rooms', function (data) {
+            var items = [];
+        $.each(data.rooms, function(index, room) {
+            items.push('<li><a href="/room/'+room.slug+'">'+room.name+'</a></li>');
+        });
+        $('.rooms-list').html(items);
+    });
+}
+
+// Highlight the current nav item
+$('a[href="'+window.location.pathname+'"]').parent('li').addClass('active');
