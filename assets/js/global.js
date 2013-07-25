e28f49b3c3d22f737ef7fed1765dabf408f61383
Max Fierke <max@maxfierke.com>
2013-07-24 21:18:20 -0500

    Fine-tune the nav list active highlighting
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,7 @@
 // If rooms list exists, let's populate it.
 if($('.rooms-list').length > 0) {
     $.getJSON('/rooms', function (data) {
-            var items = [];
+        var items = [];
         $.each(data.rooms, function(index, room) {
             items.push('<li><a href="/room/'+room.slug+'">'+room.name+'</a></li>');
         });
@@ -10,4 +10,4 @@ if($('.rooms-list').length > 0) {
 }
 
 // Highlight the current nav item
-$('a[href="'+window.location.pathname+'"]').parent('li').addClass('active');
+$('.nav a[href="'+window.location.pathname+'"]').parent('li').addClass('active');
