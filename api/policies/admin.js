c529128dd8a1ff15d84beba96490671f79aff996
Max Fierke <max@maxfierke.com>
2013-07-31 21:27:16 -0500

    Forgot I wasn't fetching an array of groups, just checking existence.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- api/policies/admin.js
+++ api/policies/admin.js
@@ -6,7 +6,7 @@ module.exports = function (req, res, ok) {
     // User is allowed, proceed to controller
     if (req.session.authenticated && req.session.user.id) {
         Group.findOne({ $and: [{ type: 'ADMIN'}, { users: req.session.user.id }] }).done(function (err, group) {
-            if (group.length > 0) {
+            if (group) {
                 return ok();
             }  else {
                 return res.send("You are not permitted to perform this action.", 403);
