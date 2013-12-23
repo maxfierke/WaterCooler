4f0929eb60e4040fa77cae1f25580cb355503cf9
Max Fierke <max@maxfierke.com>
2013-12-22 23:57:10 -0600

    Refresh subscribers on Socket.io `connect` event.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- assets/app/controllers/ChatController.js
+++ assets/app/controllers/ChatController.js
@@ -2,7 +2,7 @@ angular.module('watercooler').controller('ChatController', ['$scope', '$window',
     function ($scope, $window, socket) {
         $scope.activeUser = window.activeUser;
         $scope.room = socket.get('/room/'+$window.location.pathname.split('/')[2], function (room) { $scope.room = room; });
-        $scope.clients = socket.get('/room/'+$window.location.pathname.split('/')[2]+'/subscribers', function (res) { $scope.clients = res.users; });
+        $scope.clients = [];
         $scope.messages = socket.get('/room/'+$window.location.pathname.split('/')[2]+'/messages?limit=5', function (res) { $scope.messages = res.messages; });
         $scope.currentMessage = '';
 
@@ -15,6 +15,11 @@ angular.module('watercooler').controller('ChatController', ['$scope', '$window',
                 });
         };
 
+        socket.on('connect', function (response) {
+            socket.get('/room/'+$window.location.pathname.split('/')[2]+'/subscribers',
+                function (res) { $scope.clients = res.users; });
+        });
+
         socket.on('message', function(response) {
             if(response.model === 'message' && response.verb === 'create') {
                 $scope.messages.push(response.data);
@@ -22,11 +27,11 @@ angular.module('watercooler').controller('ChatController', ['$scope', '$window',
         });
 
         socket.on('presence', function(response) {
-            if (response.state == 'online'){
+            if (response.state === 'online'){
                 if (!_.find($scope.clients, function (client) { return client.id === response.user.id; })) {
                     $scope.clients.push(response.user);
                 }
-            } else if(response.state == 'offline'){
+            } else if(response.state === 'offline'){
                 $scope.clients = _.reject($scope.clients, function (client) { return client.id === response.user.id; });
             }
         });
