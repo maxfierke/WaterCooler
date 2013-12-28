23514bbcb2bd7fc6422f9648c27fe4e0a6451ae4
Max Fierke <max@maxfierke.com>
2013-12-28 16:31:29 -0600

    Implement active nav highlighting in Angular
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- assets/app/controllers/PageController.js
+++ assets/app/controllers/PageController.js
@@ -1,5 +1,10 @@
-angular.module('watercooler').controller('PageController', ['$scope', '$http',
-    function ($scope, $http) {
+angular.module('watercooler').controller('PageController', ['$scope', '$http', '$window',
+    function ($scope, $http, $window) {
         $scope.rooms = $http.get('/rooms').success(function (data) { $scope.rooms = data.rooms; });
         $scope.groups = $http.get('/groups').success(function (data) { $scope.groups = data.groups; });
+
+        $scope.isActive = function (viewLocation) {
+            var active = (viewLocation === $window.location.pathname);
+            return active;
+        };
     }]);
